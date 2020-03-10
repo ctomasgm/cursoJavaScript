07c9f7e8e512e5b3111e4fa08edb974d4c6f2d15
Carlos <a320605@uach.mx>
2020-03-10 11:33:25 -0700

    Actividad 10
    Se añadio la actividad 10 para mostrar una lista

--- Ejercicio 10/script.js
+++ Ejercicio 10/script.js
@@ -1,1 +1,19 @@
-const flip = (text) => text.split('').reverse().join('');
+var array=[];
+ul=document.getElementById("list")
+
+const getList = () => array.forEach((element)=> {
+
+    let li = document.createElement("li");
+    li.innerHTML +=element;
+    ul.appendChild(li);
+})
+
+const addElement = () => {
+    ul.innerHTML="";
+    array.push(document.getElementById("text").value);
+}
+
+
+
+
+
