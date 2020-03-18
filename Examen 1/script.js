48c791f5ccc9cedcb299482a1a7abe5a19639a1b
Carlos García <a320605@uach.mx>
2020-03-17 20:33:00 -0700

    time+price

--- Examen 1/script.js
+++ Examen 1/script.js
@@ -1,11 +1,16 @@
+let times = new Array(12);
+
 const changeImage = (num) => {
     if (document.getElementById(`tab${num}`).src.match('tableOff.png')) {
         document.getElementById(`tab${num}`).src = "tableOn.png";
-        document.getElementById(`strTimeT${num}`).innerHTML=(getTime(num));
+        document.getElementById(`strTimeT${num}`).innerHTML = (getTime(num));
+        document.getElementById(`priceT${num}`).innerHTML = '-';
+        document.getElementById(`endTimeT${num}`).innerHTML = " ";
     }
     else {
         document.getElementById(`tab${num}`).src = "tableOff.png"
-        document.getElementById(`endTimeT${num}`).innerHTML=(getTime(num));
+        document.getElementById(`endTimeT${num}`).innerHTML = (getTime(num));
+        document.getElementById(`priceT${num}`).innerHTML = (`$${getPrice(num)}`);
     }
 }
 
@@ -17,9 +22,18 @@ const getTime = (num) => {
 }
 
 const getPrice = (num) => {
-    let pricePerSecond = 50/3600;
-    let strTime = new Date()
-    let endTime = new Date()
-    document.getElementById(`strTimeT${num}`).innerHTML
-    document.getElementById(`endTimeT${num}`).innerHTML
+    let pricePerSecond = 50 / 3600;
+    let strTime = getDate(document.getElementById(`strTimeT${num}`).innerHTML);
+    let endTime = getDate(document.getElementById(`endTimeT${num}`).innerHTML);
+    let difSec = ((endTime.getTime() - strTime.getTime()) / 1000);
+    return difSec * pricePerSecond;
+}
+
+const getDate = (time) => {
+    var arr = time.split(":");
+    var date = new Date();
+    date.setHours(arr[0]);
+    date.setMinutes(arr[1]);
+    date.setSeconds(arr[2]);
+    return date;
 }
